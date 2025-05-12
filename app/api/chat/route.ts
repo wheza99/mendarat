a40b1f80ac3bac75f19dd9580eff63016d0cbbfd
Wheza <wheza99@gmail.com>
2025-05-13 06:39:28 +0700

    init assistant ui
    init assistant ui

---
+++ app/api/chat/route.ts
@@ -0,0 +1,23 @@
+import { openai } from "@ai-sdk/openai";
+import { frontendTools } from "@assistant-ui/react-ai-sdk";
+import { streamText } from "ai";
+
+export const runtime = "edge";
+export const maxDuration = 30;
+
+export async function POST(req: Request) {
+  const { messages, system, tools } = await req.json();
+
+  const result = streamText({
+    model: openai("gpt-4o"),
+    messages,
+    toolCallStreaming: true,
+    system,
+    tools: {
+      ...frontendTools(tools),
+      // add backend tools here
+    },
+  });
+
+  return result.toDataStreamResponse();
+}
